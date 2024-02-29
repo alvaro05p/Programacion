c71b6cc9139743ac190f994e7a637c769cd0aed3
AlvaroPardo <alvaro05pomg@gmail.com>
2024-02-29 18:04:11 +0100

    Tarea terminada

--- excepciones/Vector.java
+++ excepciones/Vector.java
@@ -3,11 +3,31 @@
 public class Vector {
     public static void main(String[] args) {
 
-        String[] array;
+        int array[] = new int[5];
+
+        try {
+
+            for (int i=0; i < 5; i++){
+
+                if (i == 3){
+
+                    throw new ArrayIndexOutOfBoundsException("No se puede asignar nada al 3er valor");
+
+                }
+                array[i]=i;
+                System.out.println(array[i]);
+
+            }
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            
+            System.out.println(e.getMessage());
+
+        }
 
         
 
-
+        
 
 
     }
