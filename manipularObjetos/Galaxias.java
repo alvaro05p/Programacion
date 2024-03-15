5efedea8b8b6cb269b9c28403344df6cb827bcd6
Alvaro Pardo <alvaro05pomg@gmail.com>
2024-03-15 18:02:53 +0100

    Falta probar errores

--- manipularObjetos/Galaxias.java
+++ manipularObjetos/Galaxias.java
@@ -1,6 +1,11 @@
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class Galaxias{
 
@@ -10,7 +15,7 @@ public class Galaxias{
 
             String[] partes;
 
-            
+            File sac = new File("SAC.bin");
 
             FileReader lector = new FileReader("SAC_DeepSky_Ver81_QCQ.TXT");
             
@@ -25,17 +30,24 @@ public class Galaxias{
 
                 Galaxia galaxy = new Galaxia(partes[0],partes[3],partes[4],partes[5],partes[6]);
 
-                
-                System.out.println(galaxy);
-                
+                if (partes[2].equals("\"GALXY\"")){
 
+                    FileOutputStream escritor = new FileOutputStream(sac,true);
+                    ObjectOutputStream escritor2 = new ObjectOutputStream(escritor);
+                
+                    escritor2.writeObject(galaxy);
+                    System.out.println(galaxy);
+                    escritor.close();
+                    escritor2.close();
+                
+                }
             }
 
-        } catch (Exception e) {
+        } catch (IOException e) {
             
             System.out.println("Error al leer el fichero");
             
-        }
+        } 
         
         
 
